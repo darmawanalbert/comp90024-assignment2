391b1e4ec6e49fab764b512a7861bd478cb58498
Albert Darmawan <darmawanalbert@gmail.com>
2021-04-18 18:53:34 +0700

    Remove unnecessary boilerplate code, dark mode enabled

---
+++ frontend/pages/_document.js
@@ -0,0 +1,37 @@
+/*
+COMP90024 Team 1
+Albert Darmawan (1168452) - darmawana@student.unimelb.edu.au
+Clarisca Lawrencia (1152594) - clawrencia@student.unimelb.edu.au
+I Gede Wibawa Cakramurti (1047538) - icakramurti@student.unimelb.edu.au
+Nuvi Anggaresti (830683) - nanggaresti@student.unimelb.edu.au
+Wildan Anugrah Putra (1191132) - wildananugra@student.unimelb.edu.au
+*/
+
+import React from 'react';
+import { ColorModeScript } from '@chakra-ui/react';
+import Document, {
+    Html, Head, Main, NextScript,
+} from 'next/document';
+import theme from '../utils/theme';
+
+class MyDocument extends Document {
+    static async getInitialProps(ctx) {
+        const initialProps = await Document.getInitialProps(ctx);
+        return { ...initialProps };
+    }
+
+    render() {
+        return (
+            <Html>
+                <Head />
+                <body>
+                    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
+    }
+}
+
+export default MyDocument;
