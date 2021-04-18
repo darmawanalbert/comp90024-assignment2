1b3f660318a41715d08c2a92683998cbb4bac744
Albert Darmawan <darmawanalbert@gmail.com>
2021-04-18 18:30:16 +0700

    Header format

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,3 +1,12 @@
+/*
+COMP90024 Team 1
+Albert Darmawan (1168452) - darmawana@student.unimelb.edu.au
+Clarisca Lawrencia (1152594) - clawrencia@student.unimelb.edu.au
+I Gede Wibawa Cakramurti (1047538) - icakramurti@student.unimelb.edu.au
+Nuvi Anggaresti (830683) - nanggaresti@student.unimelb.edu.au
+Wildan Anugrah Putra (1191132) - wildananugra@student.unimelb.edu.au
+*/
+
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
