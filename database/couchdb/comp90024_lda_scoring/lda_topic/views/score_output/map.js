cf30a551d6d0fc58fefed073dd6ef60a051bc192
cakraocha <cakraocha@gmail.com>
2021-05-20 16:33:38 +1000

    Fix views map.js for lda_scoring -> topic

--- database/couchdb/comp90024_lda_scoring/lda_topic/views/score_output/map.js
+++ database/couchdb/comp90024_lda_scoring/lda_topic/views/score_output/map.js
@@ -1,13 +1,14 @@
 function (doc) {
-    var d = doc.date; // String format such as "[2021, 5, 9]"
-    d = d.substring(1, test.length-1);
-    d = d.split(",");
-    for (i=0; i<d.length;i++){
-	    d_i = d[i].trim();
-      d_i = parseInt(d_i);
-      d[i] = d_i;
-    }
-    emit([d, doc.location], {lda_keywords: doc.lda_result, topic_score: {sport: doc.score_sports,
+  var d = doc.date // String format such as "[2021, 5, 9]"
+  d = d.replace("[", "");
+  d = d.replace("]", "");
+  d = d.split(",");
+  for (i=0; i<d.length; i++) {
+    d_i = d[i].trim();
+    d_i = parseInt(d_i);
+    d[i] = d_i;
+  }
+  emit([d, doc.location], {lda_keywords: doc.lda_result, topic_score: {sport: doc.score_sports,
       places: doc.score_places, politics: doc.score_politics, education: doc.score_education,
       entertainment: doc.score_entertainment, business: doc.score_business}});
   }
