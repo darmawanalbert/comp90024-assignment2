cfd792837e4cc8c3917b72c8f6b46cf124f12204
cakraocha <cakraocha@gmail.com>
2021-05-26 10:46:22 +1000

    Tidy up readme and file comments

--- database/destroy_db.sh
+++ database/destroy_db.sh
@@ -1,9 +1,21 @@
-docker network disconnect couchdbnet db-worker
-docker network disconnect couchdbnet db-master
-docker network rm couchdbnet
+# COMP90024 Team 1
+# Albert, Darmawan (1168452) - Jakarta, ID - darmawana@student.unimelb.edu.au
+# Clarisca, Lawrencia (1152594) - Melbourne, AU - clawrencia@student.unimelb.edu.au
+# I Gede Wibawa, Cakramurti (1047538) - Melbourne, AU - icakramurti@student.unimelb.edu.au
+# Nuvi, Anggaresti (830683) - Melbourne, AU - nanggaresti@student.unimelb.edu.au
+# Wildan Anugrah, Putra (1191132) - Jakarta, ID - wildananugra@student.unimelb.edu.au
 
-# stop
-docker container stop db-worker db-master
+# Use this file to destroy db
 
-# remove
-docker container rm db-worker db-master
+# Change the constants to the instance you wish to destroy
+source constants.sh
+export node=${INSTANCE4}
+export dockername=mastercouchdb
+
+# Disconnecting network
+sudo docker network disconnect couchdb@${node} dockername
+sudo docker network rm couchdb@${node}
+
+# Stopping and removing CouchDB docker
+sudo docker stop dockername
+sudo docker rm dockername
